97b1effcde27e0d5489006c9d6ba398fc8bd42d5
lovixk <991807272@qq.com>
2021-05-06 17:40:50 +0800

    feature: scss 兼容处理

--- gulp_test/gulpfile.js
+++ gulp_test/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const cssmin = require('gulp-cssmin');
 const autoprefixer = require('gulp-autoprefixer')
+const sass = require('gulp-sass')
 
 // 公共部分
 const cssTask = gulp
@@ -11,6 +12,14 @@ const cssTask = gulp
     .pipe(cssmin())
     .pipe(gulp.dest('./dist/css/'))
 
+const sassTask = gulp
+    .src('./src/css/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 2 versions']
+    }))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./dist/css/'))
 // 创建一个打包 CSS 任务 （gulp3.* 写法） start
 // gulp.task('cssHandler', function () {
 //     return cssTask
@@ -24,3 +33,9 @@ const cssHandler = function () {
 
 module.exports.cssHandler = cssHandler
 // 创建一个打包 CSS 任务 （gulp4.* 写法） end
+
+const scssHandler = function () {
+    return sassTask
+}
+
+module.exports.scssHandler = scssHandler
