236407026531626c901c26da4ea436ffab5ca5bf
lovixk <991807272@qq.com>
2021-05-07 11:50:15 +0800

    feature: html 压缩

--- gulp_test/gulpfile.js
+++ gulp_test/gulpfile.js
@@ -4,6 +4,9 @@ const autoprefixer = require('gulp-autoprefixer')
 const sass = require('gulp-sass')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
+const htmlmin = require('gulp-htmlmin')
+const del = require('del')
+// const rimraf = require('rimraf')
 
 // 公共部分
 const cssTask = gulp
@@ -11,6 +14,7 @@ const cssTask = gulp
     .pipe(autoprefixer({
         overrideBrowserslist: ['last 2 versions']
     }))
+
     .pipe(cssmin())
     .pipe(gulp.dest('./dist/css/'))
 
@@ -56,3 +60,47 @@ const jsHandler = function () {
 
 module.exports.jsHandler = jsHandler
 // js 压缩，支持 es新语法（es2020等） end
+
+
+// html 压缩 start
+const htmlHandler = function () {
+    return gulp
+        .src('./src/pages/*.html')
+        .pipe(htmlmin({
+            // collapseWhitespace: true // 移除空格
+            removeEmptyAttributes: true, // 移除空属性（只限于原生属性）
+            collapseBooleanAttributes: true, // 移除 checked 类似的布尔值属性
+            removeAttributeQuotes: true, // 移除属性上的双引号
+            minifyCss: true, // 压缩内嵌式css 代码（只能基本压缩，不能自动添加前缀 ）
+            minifyJS: true, // 压缩内嵌式JS 代码（只能基本压缩，不能进行转码）
+            removeStyleLinkTypeAttributes: true, // 移除 style 和link  标签上的type
+            removeScriptTypeAttributes: true // 移除script 标签上默认的type 属性
+        }))
+        .pipe(gulp.dest('./dist/html'))
+}
+
+module.exports.htmlHandler = htmlHandler
+// html 压缩 end
+
+// 删除打包文件 start
+const delHandler = function () {
+    return del(['./dist'])
+}
+
+module.exports.delHandler = delHandler
+// 删除打包文件 end
+
+// 配置默认任务，执行所有打包任务 start
+module.exports.default = function () {
+    return gulp.parallel(cssHandler, htmlHandler, jsHandler, scssHandler)
+}
+// 配置默认任务，执行所有打包任务 end
+
+// 创建一个 启动服务器的任务 start
+const webHandler = function () {
+    return gulp
+        .src()
+}
+// 创建一个 启动服务器的任务 end
+
+
